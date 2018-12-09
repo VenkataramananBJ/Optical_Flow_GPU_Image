49372cb4b227917597e9fbf0b1be3e374b3e4c1e
dvveera <d.v.veera@gmail.com>
2018-12-09 17:03:11 -0500

    Initial

---
+++ build/CMakeFiles/Optical_Flow_GPU_Image.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Optical_Flow_GPU_Image.dir/Optical_Flow_GPU_Image.cpp.o"
+  "Optical_Flow_GPU_Image.pdb"
+  "Optical_Flow_GPU_Image"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Optical_Flow_GPU_Image.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
